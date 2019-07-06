9cdc865ac16e029a0c10639d0555c6f7d49514fe
Ahmed-Hamouda <ahr@odootec.com>
2019-07-06 11:14:11 +0200

    date format

--- odt_zawaj_hr/models/contract.py
+++ odt_zawaj_hr/models/contract.py
@@ -1,12 +1,14 @@
 from odoo import models, api, fields
 from datetime import datetime
 
+
 class EmployeeContract(models.Model):
     _inherit = 'hr.contract'
 
-    name = fields.Char('Contract Reference', readonly=True,compute="get_ref")
+    name = fields.Char('Contract Reference', readonly=True, compute="get_ref")
     gossi_reg_no = fields.Char('Gossi Registration Number')
-    total_salary = fields.Monetary('Total Salary', digits=(16, 2), compute='_compute_total_salary', track_visibility="onchange", help="Employee's monthly Total Salary.")
+    total_salary = fields.Monetary('Total Salary', digits=(16, 2), compute='_compute_total_salary',
+                                   track_visibility="onchange", help="Employee's monthly Total Salary.")
     zw_idara = fields.Many2one(related='employee_id.zw_idara', string='Location')
     analytic_tag_ids = fields.Many2many('account.analytic.tag', string='Analytic Tags')
 
