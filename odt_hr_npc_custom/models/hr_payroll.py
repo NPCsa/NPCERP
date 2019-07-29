d69dbbf1222599f2b491074a0d146b9591840785
Ahmed-Hamouda <ahr@odootec.com>
2019-07-29 10:51:41 +0200

    absence calculaion

--- odt_hr_npc_custom/models/hr_payroll.py
+++ odt_hr_npc_custom/models/hr_payroll.py
@@ -3,11 +3,14 @@
 from __future__ import division
 from dateutil import relativedelta
 from odoo import api, fields, models, tools, _
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import UserError
+from datetime import datetime,date
 
 
 class Payslip(models.Model):
     _inherit = 'hr.payslip'
+
+    absent_times = fields.Integer(string="Absent Times",  required=False )
 
     struct_id = fields.Many2one('hr.payroll.structure', string='Structure', related='contract_id.struct_id',
                                 readonly=True, states={'draft': [('readonly', False)]},
@@ -16,6 +19,70 @@ class Payslip(models.Model):
                                      'mandatory anymore and thus the rules applied will be all the rules set on the '
                                      'structure of all contracts of the employee valid for the chosen period')
 
+
+    @api.one
+    def _compute_absent_days(self):
+        for record in self:
+            f_time = 0
+            s_time = 0
+            t_time = 0
+            date_from = fields.Datetime.from_string(record.date_from)
+            start_date = fields.Date.to_string(date_from.replace(day=1,month=1))
+            payslips = self.search([('employee_id','=',record.employee_id.id),('date_from','>=',start_date),('date_from','<',date_from)])
+
+            for rule in payslips.mapped('worked_days_line_ids'):
+                if rule.code == 'Absence':
+                    if 1 <= rule.number_of_days <= 6:
+                        f_time += 1
+                    elif 7 <= rule.number_of_days <= 10:
+                        s_time += 1
+                    elif 11 <= rule.number_of_days <= 14:
+                        t_time += 1
+                    else:
+                        if rule.number_of_days != 0:
+                            t_time = 3
+            for rrule in record.mapped('worked_days_line_ids'):
+                if rrule.code == 'Absence':
+                    if 1 <= rrule.number_of_days <= 6:
+                        f_time += 1
+                        if f_time == 1:
+                            record.absent_times = 11
+                        elif f_time == 2:
+                            record.absent_times = 12
+                        elif f_time == 3:
+                            record.absent_times = 13
+                        else:
+                            if f_time != 0:
+                                record.absent_times = 13
+
+                    elif 7 <= rrule.number_of_days <= 10:
+                        s_time += 1
+                        if s_time == 1:
+                            record.absent_times = 21
+                        elif s_time == 2:
+                            record.absent_times = 22
+                        elif s_time == 3:
+                            record.absent_times = 23
+                        else:
+                            if s_time != 0:
+                                record.absent_times = 23
+                    elif 11 <= rrule.number_of_days <= 14:
+                        t_time += 1
+                        if t_time == 1:
+                            record.absent_times = 31
+                        elif t_time == 2:
+                            record.absent_times = 32
+                        else:
+                            if t_time != 0:
+                                record.absent_times = 32
+            print('------------------self.absent_times--------------',self.absent_times)
+            # if record.absent_times in [23,32]:
+            #     raise UserError(_('You can not Compute Payslip Absence Days Exceeded %s' %record.employee_id.name))
+    @api.multi
+    def compute_sheet(self):
+        for record in self:
+            record._compute_absent_days()
+        return super(Payslip, self).compute_sheet()
 
     @api.model
     def create(self, values):
