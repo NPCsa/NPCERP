42b61859636e2285003ea0bb5b7c400d8bb2842e
s7s <hussainali2233>
2019-10-07 09:16:59 +0200

    add bank commission

---
+++ odt_bank_commission/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':"odt Bank Commission",
+    'summary':""" """,
+    'discription':"odt Bank Commission",
+    'author':"odotech",
+    'website':" ",
+    'category':'',
+    'version':'',
+    'depends': ['base','account'],
+    'data':[
+        'views/account_payment_view.xml',
+        'views/account_tax_view.xml'
+
+    ]
+
+}
