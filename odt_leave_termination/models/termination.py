0925e0093e40cced6d3eb99f78197bf20eada88b
Ahmad-Hamouda <33389047+Ahmad-Hamouda@users.noreply.github.com>
2019-11-17 15:17:17 +0200

    [UPDT] include gosi settlment

--- odt_leave_termination/models/termination.py
+++ odt_leave_termination/models/termination.py
@@ -304,7 +304,8 @@ class Settlement(models.Model):
                     elif field.name == 'other_allowance':
                         salary_amount += (basic * (
                                 record.contract_id.other_allowance / 100) if record.contract_id.is_other else record.contract_id.other_allowance)
-
+                    elif field.name == 'gosi':
+                        salary_amount += record.contract_id.gosi
             record.salary_amount = salary_amount
             remaining_vacation = 0.0
             if record.reconcile_type == 'request':
