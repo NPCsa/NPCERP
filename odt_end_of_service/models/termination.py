e00371805ff92863584c7728295e4ffcf8739045
Ahmed <ahr@odootec.com>
2019-12-02 11:54:35 +0200

    Remove Pyc files 2

--- odt_end_of_service/models/termination.py
+++ odt_end_of_service/models/termination.py
@@ -1,7 +1,7 @@
 import time
 from odoo import exceptions, models, fields, api, _
 from dateutil.relativedelta import relativedelta
-from datetime import datetime
+from datetime import datetime,timedelta
 from .. import utils
 from odoo.exceptions import Warning
 
@@ -234,7 +234,7 @@ class Termination(models.Model):
             start_date = datetime.strptime(str(self.hire_date), '%Y-%m-%d')
             end_date = datetime.strptime(str(self.job_ending_date), '%Y-%m-%d')
             # months = utils.months_between(start_date, end_date)
-            years = utils.years_between(start_date, end_date)
+            # years = utils.years_between(start_date, end_date)
             # self.period_in_years = years
             diff = relativedelta(end_date + timedelta(days=1), start_date)
             year = diff.years
