505ab07231a2f3e2e88218ba2b3434440c99d640
Ahmed hamouda <ahr@odootec.com>
2020-01-12 15:38:27 +0200

    [ADD] add field on purchase order

---
+++ odt_purchase_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vendor Invoice No in Purchase",
+    'summary': """ Vendor Invoice No in Purchase""",
+    'description': """
+        Vendor Invoice No in Purchase
+ """,
+    'author': "OdooTec",
+    'website': "www.odootec.com",
+    'category': 'purchase',
+    'version': '1.0.1',
+    'depends': ['purchase'],
+    'data': [
+        "views/purchase_order_view.xml",
+        
+
+    ],
+    'installable': True,
+}
