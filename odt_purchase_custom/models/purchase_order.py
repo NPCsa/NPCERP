1c77dca84ae9567947637de4258ffe27e69c4d24
Ahmed hamouda <ahr@odootec.com>
2020-01-12 15:42:52 +0200

    [ADD] add field on purchase order 2

--- odt_purchase_custom/models/purchase_order.py
+++ odt_purchase_custom/models/purchase_order.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    vendor_invoice_number = fields.Char(string='Vendor Invoice No',required=True)
+    vendor_invoice_number = fields.Char(string='Vendor Invoice No',required=False)
 
     _sql_constraints = [
         ('vendor_invoice_number_uniq',
