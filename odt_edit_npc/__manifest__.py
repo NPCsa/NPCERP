c133a63a60b5fbbcbbd2de03834cf081db4e2821
amr.hesham <amr.hesham@odootec.com>
2020-06-02 18:14:03 +0200

    edit_reports

--- odt_edit_npc/__manifest__.py
+++ odt_edit_npc/__manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','sale','sale_management','purchase'],
+    'depends': ['base','account','sale','sale_management','purchase','stock'],
 
     # always loaded
     'data': [
